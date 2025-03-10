b124582743c9c2d31148ab961e5843f28f5db886
joaoeudes7 <joaoeudes7@gmail.com>
2025-03-10 01:27:08 -0300

    added safe exit

--- app/src/main/java/com/jedev/simulator/simulator/order/NavigationApp.kt
+++ app/src/main/java/com/jedev/simulator/simulator/order/NavigationApp.kt
@@ -1,6 +1,13 @@
 package com.jedev.simulator.simulator.order
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalActivity
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,6 +17,7 @@ import com.jedev.simulator.simulator.order.Destinations.Home
 import com.jedev.simulator.simulator.order.ui.features.editOrder.EditOrderScreen
 import com.jedev.simulator.simulator.order.ui.features.home.HomeScreen
 import kotlinx.serialization.Serializable
+import java.util.concurrent.TimeUnit
 
 @Serializable
 sealed class Destinations {
@@ -26,6 +34,36 @@ fun AppNavHost(
     startDestination: Destinations
 ) {
     val navController = rememberNavController()
+    val activity = LocalActivity.current
+
+    var lastExitRequest by remember {
+        mutableLongStateOf(0L)
+    }
+
+    fun onBack() {
+        navController.popBackStack()
+    }
+
+    BackHandler {
+        when {
+            navController.previousBackStackEntry == null -> {
+                val now = System.currentTimeMillis()
+                val timeoutExit = now - lastExitRequest < TimeUnit.SECONDS.toMillis(5)
+
+                lastExitRequest = now
+
+                var msgToast = activity?.getString(R.string.press_back_again)
+
+                if (timeoutExit) {
+                    msgToast = activity?.getString(R.string.safe_exit_finished)
+                    activity?.finish()
+                }
+
+                Toast.makeText(activity, msgToast, Toast.LENGTH_SHORT).show()
+            }
+            else -> onBack()
+        }
+    }
 
     NavHost(
         modifier = modifier,
